f1c405a64e1cb51e90b9c3037abbc42b49b182ad
minhtan <minhtan471100@gmail.com>
2020-10-09 14:21:11 +0700

    ready to go

---
+++ advanced-component-usage/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <appQuote quote="A wonderful Quote"></appQuote>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Quote from "./components/Quote";
+
+export default {
+  components: {
+    appQuote: Quote,
+  },
+};
+</script>
+
+<style scoped></style>>
---
+++ advanced-component-usage/src/components/Quote.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <p>{{ quote }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "Quote",
+    };
+  },
+  props: ['quote']
+};
+</script>
+
+<style scoped>
+div {
+  padding: 30px;
+  margin: auto;
+  text-align: center;
+  border: 1px solid #ccc;
+  box-shadow: 1px 1px 2px black;
+}
+</style>
